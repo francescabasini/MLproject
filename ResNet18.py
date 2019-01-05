d1b3bee3edfb61617f13dbda59ed27590717fe3f
basins95 <41323954+basins95@users.noreply.github.com>
2019-01-05 23:50:19 +0100

    Riccardo's code for ResNet18

---
+++ ResNet18.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import matplotlib.image as mpimg
+import random
+import sys
+from datetime import datetime
+# due righe sotto per caricare qualsiasi risoluzione di immagine
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = None
+
+
+# creo il dataset contenente i nomi dei file .jpg da leggere e le rispettive labels
+all_data_info_true300 = pd.read_csv("C:\\Users\\basi9\\Desktop\\ML Project\\Data\\all_data_info_true300.csv")
+
+all_data_info_true300_count = all_data_info_true300.groupby('artist').count()
+print(all_data_info_true300_count.shape)
+artist_list = all_data_info_true300_count.index.values.tolist()
+
+## Image processing to get the starting data for training the model
+from keras.preprocessing.image import ImageDataGenerator
+
+df = all_data_info_true300.loc[:, ['artist','new_filename']]
+
+train_datagen = ImageDataGenerator(horizontal_flip=True)
+valid_datagen = ImageDataGenerator(horizontal_flip=False)
+#featurewise_center=True   0-center
+#featurewise_std_normalization    normalize
+
+train_generator = train_datagen.flow_from_dataframe(df,\
+"C:\\Users\\basi9\\Desktop\\ML Project\\Data\\train", \
+target_size=(224, 224), x_col='new_filename',\
+y_col='artist', has_ext=True, seed=100)
+#Found 13680 images belonging to 57 classes.
+
+valid_generator = valid_datagen.flow_from_dataframe(df,\
+"C:\\Users\\basi9\\Desktop\\ML Project\\Data\\valid",\
+target_size=(224, 224), x_col='new_filename',\
+y_col='artist', has_ext=True, seed=100)
+#Found 1710 images belonging to 57 classes.
+
+#color_mode='rgb' default
+#has_ext has been deprecated, extensions included
+#class_mode= default categorical
+#batch_size: size of the batches of data (default: 32)
+
+STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
+#i.e. 13680//32 = 427
+STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
+
+
+###   ResNet18 - transfer learning   ###
+
+# transfer learning
+from classification_models import ResNet18
+# build model
+n_classes = len(artist_list)
+base_model = ResNet18(input_shape=(224,224,3), weights='imagenet', include_top=False)
+x = keras.layers.GlobalAveragePooling2D()(base_model.output)
+output = keras.layers.Dense(n_classes, activation='softmax')(x)
+model = keras.models.Model(inputs=[base_model.input], outputs=[output])
+model.summary()
+# ora è transfer learning, infatti setto i primi layer del modello a 'già trainati'
+#len(model.layers)
+#model.layers[(len(model.layers) - 2) : len(model.layers)]
+#for i in range(len(model.layers) - 2):
+#    model.layers[i].trainable = False
+#model.summary()
+
+from keras.metrics import top_k_categorical_accuracy
+from keras.callbacks import ReduceLROnPlateau
+def top_1_categorical_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=1)
+def top_3_categorical_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=3)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',\
+                                            factor=0.1,\
+                                            patience=3,\
+                                            verbose=1,\
+                                            min_lr=0.0001)
+model.compile(optimizer='Adam', loss='categorical_crossentropy',\
+              metrics=['accuracy', top_1_categorical_accuracy, top_3_categorical_accuracy])
+              
+# training
+model.compile(optimizer='Adam', loss='categorical_crossentropy',\
+              metrics=['accuracy', top_1_categorical_accuracy, top_3_categorical_accuracy])
+model.fit_generator(generator=train_generator,\
+                    steps_per_epoch=STEP_SIZE_TRAIN,\
+                    validation_data=valid_generator,\
+                    validation_steps=STEP_SIZE_VALID,\
+                    epochs=30, verbose=2,callbacks=[learning_rate_reduction])
+model.save('C:\\Users\\basi9\\Desktop\\ML Project\\Data\\ResNet18_finetune.h5')  # TL sta per 'Transfer Learning'
+
+# list all data in history
+print(model.history.history.keys())
+# summarize history for accuracy
+plt.plot(model.history.history['acc'])
+plt.plot(model.history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
