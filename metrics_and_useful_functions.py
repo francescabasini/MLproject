6e02eef3bf69abf178cff0ca7d93108d544c55e2
RicSpd <46091334+RicSpd@users.noreply.github.com>
2019-01-13 13:53:27 +0100

    Update metrics_and_useful_functions.py

--- metrics_and_useful_functions.py
+++ metrics_and_useful_functions.py
@@ -108,6 +108,31 @@ def crop_image(img):
     # now random 224x224 crop
     rand_numb_0 = random.randint(0, im.shape[0] - 224)
     rand_numb_1 = random.randint(0, im.shape[1] - 224)
-    im = im[rand_numb_0:(rand_numb_0 + 224), rand_numb_1:(rand_numb_1 + 224), :]
+    im = im[rand_numb_0:(rand_numb_0 + 224), rand_numb_1:(rand_numb_1 + 224), :] # random crop
+    # add one dimension for prediction
     im = np.expand_dims(im, axis=0)
+    return im
+
+def crop_image_train(img):
+    im = img
+    if len(im.shape) < 3:  # make black&white images RGB
+        im = np.stack((im, im, im), -1)
+    if im.shape[2] == 4:  # remove 4th channel if present
+        im = im[:, :, 0:3]
+    # now random 224x224 crop
+    rand_numb_0 = random.randint(0, im.shape[0] - 224)
+    rand_numb_1 = random.randint(0, im.shape[1] - 224)
+    im = im[rand_numb_0:(rand_numb_0 + 224), rand_numb_1:(rand_numb_1 + 224), :]
+    return im
+
+def crop_image_valid(img):
+    im = img
+    if len(im.shape) < 3:  # make black&white images RGB
+        im = np.stack((im, im, im), -1)
+    if im.shape[2] == 4:  # remove 4th channel if present
+        im = im[:, :, 0:3]
+    # now 224x224 crop at the center
+    numb_0 = im.shape[0] // 2 - 112
+    numb_1 = im.shape[1] // 2 - 112
+    im = im[numb_0:(numb_0 + 224), numb_1:(numb_1 + 224), :]
     return im
