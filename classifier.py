48308c1b7719bafc460891a4ca5bf2f2f8353bf0
RicSpd <46091334+RicSpd@users.noreply.github.com>
2019-01-13 16:34:37 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -4,7 +4,7 @@ import matplotlib.pylab as plt
 import os
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
-from metrics_and_useful_functions import crop_image
+from metrics_and_useful_functions import crop_image, crop_image_train
 
 
 
@@ -21,36 +21,82 @@ img_number = str(input('Enter the image file number: '))
 while (img_number + '.jpg') not in paint_57artists_list:
     print('Entered painting is not from one of the 57 artists.')
     img_number = str(input('Please enter another image file number: '))
-# show the entire original painting
-img = plt.imread(os.path.join(work_dir, 'all_paintings', img_number + '.jpg'))
-plt.figure()
-plt.imshow(img)
+# # show the entire original painting
+# img = plt.imread(os.path.join(work_dir, 'all_paintings', img_number + '.jpg'))
+# plt.figure()
+# plt.imshow(img)
+#
+# # take a random crop of the image
+# img = crop_image(img)
+# plt.figure()
+# plt.imshow(img[0])
 
-# take a random crop of the image
-img = crop_image(img)
-plt.figure()
-plt.imshow(img[0])
+
+
+# ###   Load model and predict author   ###
+# from metrics_and_useful_functions import top_3_categorical_accuracy
+# from keras.models import load_model
+# from keras.preprocessing.image import ImageDataGenerator
+#
+# model = load_model(os.path.join(work_dir, 'RN18_finetuning_2nd_step.h5'),   # change according to your saved model name
+#                    custom_objects={'top_3_categorical_accuracy': top_3_categorical_accuracy})
+#
+# # generators
+# valid_datagen = ImageDataGenerator(horizontal_flip=False)
+# valid_generator = valid_datagen.flow(img, y=None, batch_size=1, shuffle=False, seed=100)
+#
+# # predict
+# valid_generator.reset()
+# preds = model.predict_generator(generator=valid_generator, steps=len(valid_generator), verbose=0)
+# top_1_pred = np.argmax(preds, axis=1)
+# top_3_pred = np.argsort(-preds)[0][:3]
+#
+# # get top-3 predicted authors
+# results = [(artist_list[k], round(preds[0,k], 3)) for k in top_3_pred]
+# print(results)
+# print('The true author of painting ' + img_number + ' is:   ' +
+#       df_57artists[df_57artists['new_filename'] == (img_number + '.jpg')].artist.to_string(index=False))
 
 
 
-###   Load model and predict author   ###
-from metrics_and_useful_functions import top_3_categorical_accuracy, precision, recall, F1_score
+# ### Use predict instead of predict_generator
+# from metrics_and_useful_functions import top_3_categorical_accuracy
+# from keras.models import load_model
+#
+# model = load_model(os.path.join(work_dir, 'RN18_finetuning_differentcrops_2nd_step.h5'),   # change according to your saved model name
+#                    custom_objects={'top_3_categorical_accuracy': top_3_categorical_accuracy})
+#
+# preds = model.predict(img, batch_size=None, verbose=0)
+# top_1_pred = np.argmax(preds, axis=1)
+# top_3_pred = np.argsort(-preds)[0][:3]
+#
+# # get top-3 predicted authors
+# results = [(artist_list[k], round(preds[0,k], 3)) for k in top_3_pred]
+# print(results)
+# print('The true author of painting ' + img_number + ' is:   ' +
+#       df_57artists[df_57artists['new_filename'] == (img_number + '.jpg')].artist.to_string(index=False))
+
+
+
+### con flow_from_dataframe
+from metrics_and_useful_functions import top_3_categorical_accuracy
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
-model = load_model(os.path.join(work_dir, 'RN18_from_scratch_30epoch.h5'),
-                         custom_objects={'top_3_categorical_accuracy': top_3_categorical_accuracy,
-                                         'precision': precision,
-                                         'recall': recall,
-                                         'F1_score': F1_score})
+model = load_model(os.path.join(work_dir, 'RN18_finetuning_differentcrops_2nd_step.h5'),   # change according to your saved model name
+                   custom_objects={'top_3_categorical_accuracy': top_3_categorical_accuracy})
 
 # generators
-valid_datagen = ImageDataGenerator(horizontal_flip=False)
-valid_generator = valid_datagen.flow(img, y=None, batch_size=1, shuffle=False, seed=100)
+test_datagen = ImageDataGenerator(horizontal_flip=True,
+                                  preprocessing_function=crop_image_train)
+df_one_img = df_57artists[df_57artists['new_filename'] == str(img_number + '.jpg')].reset_index(drop=True)
+test_generator = test_datagen.flow_from_dataframe(df_one_img, os.path.join(work_dir, 'all_paintings'),
+                            target_size=(224, 224), x_col='new_filename', y_col='artist', has_ext=True, seed=100,
+                            batch_size=1, shuffle=False, class_mode=None)
 
 # predict
-valid_generator.reset()
-preds = model.predict_generator(generator=valid_generator, steps=len(valid_generator), verbose=0)
+test_generator.reset()
+preds = model.predict_generator(generator=test_generator, steps=len(test_generator), verbose=0)
 top_1_pred = np.argmax(preds, axis=1)
 top_3_pred = np.argsort(-preds)[0][:3]
 
@@ -59,3 +105,10 @@ results = [(artist_list[k], round(preds[0,k], 3)) for k in top_3_pred]
 print(results)
 print('The true author of painting ' + img_number + ' is:   ' +
       df_57artists[df_57artists['new_filename'] == (img_number + '.jpg')].artist.to_string(index=False))
+
+# show the original image and the cropped one used in training
+img = plt.imread(os.path.join(work_dir, 'all_paintings', img_number + '.jpg'))
+plt.figure()
+plt.imshow(img)
+plt.figure()
+plt.imshow(crop_image_train(img))
